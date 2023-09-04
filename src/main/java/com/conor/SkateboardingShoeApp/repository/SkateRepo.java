1f4cf2a6244d285d7eff1e58986fc29ceb4069f1
apprentices1-admin <conor.lynam@zinkworks.com>
2023-09-04 20:43:03 +0100

    Second commit

---
+++ src/main/java/com/conor/SkateboardingShoeApp/repository/SkateRepo.java
@@ -0,0 +1,8 @@
+package com.conor.SkateboardingShoeApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkateRepo extends JpaRepository {
+}
